ed4eabce0121a93e955e82d97c1e95fc1c929cac
Ubuntu <ubuntu@ip-172-31-8-200.ap-northeast-2.compute.internal>
2023-02-27 04:19:05 +0000

    console not come up for some reason-20230227

---
+++ start-geth.sh
@@ -0,0 +1,10 @@
+./build/bin/geth \
+--nousb \
+--http \
+--http.port "8545" \
+--http.vhosts "*" \
+--http.corsdomain "*" \
+--http.api "admin,db,eth,net,web3,miner,personal" \
+--gcmode "full" \
+--syncmode "full" \
+console 
