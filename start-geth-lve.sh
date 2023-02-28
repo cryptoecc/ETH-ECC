373b3c3c65b4fe148d7c33bc6cb8e58d3739f194
Ubuntu <ubuntu@ip-172-31-8-200.ap-northeast-2.compute.internal>
2023-02-28 08:47:56 +0000

    start script working-20230228

--- start-geth-lve.sh
+++ start-geth-lve.sh
@@ -5,7 +5,13 @@
 --http.vhosts "*" \
 --http.corsdomain "*" \
 --http.api "admin,db,eth,net,web3,miner,personal" \
+--http.addr "0.0.0.0" \
 --gcmode "full" \
 --syncmode "full" \
 --lve \
-console 
+--ws --ws.port 8546 --ws.api eth,net,web3 --ws.origins '*' --ws.addr 0.0.0.0 \
+console
+
+# --rpcaddr 127.0.0.1  
+# --rpc --rpcapi db,eth,net,web3,personal --cache=2048  --rpcport 8547 --rpccorsdomain "*" \
+# --http.addr "127.0.0.1" \
